f9929c277c039817c526fffbb034220766e826b9
Gavinwgq <henuboy@live.com>
2020-03-09 20:06:33 +0800

    单链表按编号顺序插入

--- src/main/java/linkedlist/SingleLinkedListDemo.java
+++ src/main/java/linkedlist/SingleLinkedListDemo.java
@@ -14,10 +14,17 @@ public class SingleLinkedListDemo {
         HeroNode hero4 = new HeroNode(4,"林冲","豹子头");
 
         SignLinkedList signLinkedList = new SignLinkedList();
-        signLinkedList.add(hero1);
+        /*signLinkedList.add(hero1);
         signLinkedList.add(hero2);
         signLinkedList.add(hero3);
-        signLinkedList.add(hero4);
+        signLinkedList.add(hero4);*/
+
+        //按编号顺序添加
+        signLinkedList.addByOrder(hero1);
+        signLinkedList.addByOrder(hero3);
+        signLinkedList.addByOrder(hero4);
+        signLinkedList.addByOrder(hero2);
+        //signLinkedList.addByOrder(hero2);
 
         signLinkedList.list();
     }
@@ -40,6 +47,31 @@ class SignLinkedList{
         }
         //当遍历结束时temp指向最后一个节点
         temp.next = heroNode;
+    }
+
+    //按照heronode的no属性大小进行添加，如果编号已存在提示添加失败
+    public void addByOrder(HeroNode heroNode){
+        HeroNode temp = head;
+        boolean flag = false;
+        while (true){
+            if (temp.next ==null){
+                break;//已到链表尾部
+            }
+            if(temp.next.no>heroNode.no){
+                break;//位置找到
+            }
+            if(temp.next.no == heroNode.no){
+                flag = true;
+                break;//编号已存在
+            }
+            temp = temp.next;
+        }
+        if(flag){
+            System.out.printf("英雄编号%d已经存在，不能加入了\n",heroNode.no);
+        }else{
+            heroNode.next = temp.next;
+            temp.next = heroNode;
+        }
     }
 
     //遍历输出
