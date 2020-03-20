2e7d64e22743523224b59946293d66b960b3fc55
Gavinwgq <henuboy@live.com>
2020-03-20 20:38:18 +0800

    插入排序

---
+++ src/main/java/sort/InsertSort.java
@@ -0,0 +1,29 @@
+package sort;
+
+/**
+ * 插入排序
+ * @author wangguoqiang
+ * @date 2020/3/20 20:33
+ */
+public class InsertSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,4,1,6,9,7,8};
+        insertSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d\t",arr[i]);
+        }
+    }
+
+    public static void insertSort(int[] arr){
+        for (int i = 1; i < arr.length; i++) {
+            int insertVal = arr[i];//待插入的值
+            int insertIndex = i-1;//待插入的位置
+            while (insertIndex >= 0 && insertVal < arr[insertIndex]){
+                //数据后移
+                arr[insertIndex+1] = arr[insertIndex];
+                insertIndex--;
+            }
+            arr[insertIndex+1] = insertVal;
+        }
+    }
+}
