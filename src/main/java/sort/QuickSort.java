b823f87232f9f3235d4748e1d8081f97ed12fc3f
Gavinwgq <henuboy@live.com>
2020-03-21 17:07:18 +0800

    快速排序

---
+++ src/main/java/sort/QuickSort.java
@@ -0,0 +1,59 @@
+package sort;
+
+/**
+ * 快速排序
+ * @author wangguoqiang
+ * @date 2020/3/21 16:04
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {3,5,2,6,4,1,9,7,8};
+        quickSort(arr,0,arr.length-1);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%d\t",arr[i]);
+        }
+    }
+
+    public static void quickSort(int[] arr,int left,int right){
+        int l = left;
+        int r = right;
+        int middle = arr[(left+right)/2];//数组中间的值
+        int temp = 0;
+        while (l<r){
+            //从左边开始找比中间值大的，找到后退出
+            while (arr[l]<middle){
+                l++;
+            }
+            //在右边倒着找比中间值小的，找到后退出
+            while (arr[r]>middle){
+                r--;
+            }
+
+            if(l>=r){
+                //此时middle左边的都比middle小，middle右边的都比middle大
+                break;
+            }
+            temp = arr[l];
+            arr[l] = arr[r];
+            arr[r] = temp;
+
+            if(arr[l] == middle){
+                r--;
+            }
+            if(arr[r] == middle){
+                l++;
+            }
+        }
+        if(l == r){
+            l++;
+            r--;
+        }
+
+        if(left<r){
+            quickSort(arr,left,r);
+        }
+        if(right >l){
+            quickSort(arr,l,right);
+        }
+    }
+}
