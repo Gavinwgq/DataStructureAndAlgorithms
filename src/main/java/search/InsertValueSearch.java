62b23ffe5cc232872c03fc6c185abef1f776ef25
Gavinwgq <henuboy@live.com>
2020-03-25 19:10:24 +0800

    插值查找

--- src/main/java/search/InsertValueSearch.java
+++ src/main/java/search/InsertValueSearch.java
@@ -5,55 +5,61 @@ import com.google.common.collect.Lists;
 import java.util.List;
 
 /**
- * 二分查找
+ * 插值查找
+ * 对于数据量比较大，分布较均匀的数组进行查找，效果比较好
  * @author wangguoqiang
- * @date 2020/3/24 21:27
+ * @date 2020/3/25 18:53
  */
-public class BinarySearch {
+public class InsertValueSearch {
 
     public static void main(String[] args) {
-        int[] arr = {1,21,34,47,59,61,72};
+        int[] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = i+1;
+        }
         //int i = binarySearch(arr, 0, arr.length - 1, 61);
-        int i = binarySearch(arr,61);
+        int i = insertValueSearch(arr,100);
         if(i == -1){
             System.out.println("未找到数据");
         }else{
             System.out.println("数据找到,下标为"+i);
         }
-
-        System.out.println("################");
-
-        int[] arr2 = {1,21,34,47,47,47,59,61,72};
-        List<Integer> list = binarySearch3(arr2, 0, arr2.length - 1, 47);
-        System.out.println(list);
     }
 
     //递归方式
-    public static int binarySearch(int[] arr,int left,int right,int val){
+    public static int insertValueSearch(int[] arr,int left,int right,int val){
+        //val<arr[0] || val>arr[right] 条件必须要有，否在 mid 可能越界
         if(left>right || val<arr[0] || val>arr[right]){
             return -1;
         }
-        int mid = (left + right) / 2;
+        int mid = left + (right-left)*(val-arr[left])/(arr[right]-arr[left]);
         if(val>arr[mid]){
-            return binarySearch(arr,mid+1,right,val);
+            return insertValueSearch(arr,mid+1,right,val);
         }else if (val == arr[mid]){
             return mid;
         }else{
-            return binarySearch(arr,left,mid-1,val);
+            return insertValueSearch(arr,left,mid-1,val);
         }
     }
 
     //循环方式
-    public static int binarySearch(int[] arr,int val){
+    public static int insertValueSearch(int[] arr,int val){
+        if(val<arr[0] || val>arr[arr.length-1]){
+            return -1;
+        }
         int left = 0,right = arr.length-1;
         int mid = 0;
+        int count = 0;
         while (left<right){
-            mid = (left + right) / 2;
+            count++;
+            //mid = (left+right)/2;
+            mid = left + (right-left)*(val-arr[left])/(arr[right]-arr[left]);
             if(val>arr[mid]){
                 left = mid +1;
             }else if(val < arr[mid]){
                 right = mid - 1;
             }else{
+                System.out.println("循环次数："+count);
                 return mid;
             }
         }
@@ -62,13 +68,13 @@ public class BinarySearch {
 
 
     //数组中存在多个时,找出所有索引
-    public static List<Integer> binarySearch3(int[] arr, int left, int right, int val){
-        if(left>right){
+    public static List<Integer> insertValueSearch3(int[] arr, int left, int right, int val){
+        if(left>right || val<arr[0] || val>arr[right]){
             return Lists.newArrayList();
         }
-        int mid = (left + right) / 2;
+        int mid = left + (right-left)*(val-arr[left])/(arr[right]-arr[left]);
         if(val>arr[mid]){
-            return binarySearch3(arr,mid+1,right,val);
+            return insertValueSearch3(arr,mid+1,right,val);
         }else if (val == arr[mid]){
             List<Integer> result = Lists.newArrayList();
             //先从mid向左查找
@@ -91,7 +97,7 @@ public class BinarySearch {
             }
             return result;
         }else{
-            return binarySearch3(arr,left,mid-1,val);
+            return insertValueSearch3(arr,left,mid-1,val);
         }
     }
 }
