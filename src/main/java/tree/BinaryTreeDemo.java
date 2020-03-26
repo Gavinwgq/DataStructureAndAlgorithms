02ea117317b02b64eb6d8a62122669e9f04a9d51
Gavinwgq <henuboy@live.com>
2020-03-26 21:08:58 +0800

    二叉树遍历

---
+++ src/main/java/tree/BinaryTreeDemo.java
@@ -0,0 +1,140 @@
+package tree;
+
+import java.util.Hashtable;
+
+/**
+ * 二叉树
+ * @author wangguoqiang
+ * @date 2020/3/26 20:54
+ */
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        HeroNode root = new HeroNode(1,"宋江");
+        HeroNode heroNode2 = new HeroNode(2,"卢俊义");
+        HeroNode heroNode3 = new HeroNode(3,"吴用");
+        HeroNode heroNode4 = new HeroNode(4,"林冲");
+        root.setLeft(heroNode2);
+        root.setRight(heroNode3);
+        heroNode3.setRight(heroNode4);
+        binaryTree.setRoot(root);
+        System.out.println("-----前序遍历----");
+        binaryTree.preOrder();
+        System.out.println("-----中序遍历----");
+        binaryTree.midOrder();
+        System.out.println("-----后序遍历----");
+        binaryTree.lastOrder();
+    }
+}
+
+class BinaryTree{
+    private HeroNode root;
+
+    public void setRoot(HeroNode root) {
+        this.root = root;
+    }
+    //前序遍历
+    public void preOrder(){
+        if(root != null){
+            root.preOrder();
+        }
+    }
+    //中序遍历
+    public void midOrder(){
+        if(root != null){
+            root.midOrder();
+        }
+    }
+
+    //后序遍历
+    public void lastOrder(){
+        if(root != null){
+            root.lastOrder();
+        }
+    }
+}
+
+class HeroNode{
+    private int no;
+    private String name;
+    private HeroNode left;
+    private HeroNode right;
+
+    public HeroNode(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    //前序遍历
+    public void preOrder(){
+        System.out.println(this);
+        if(this.left!=null){
+            this.left.preOrder();
+        }
+        if(this.right!=null){
+            this.right.preOrder();
+        }
+    }
+
+    //中序遍历
+    public void midOrder(){
+        if(this.left!=null){
+            this.left.midOrder();
+        }
+        System.out.println(this);
+        if(this.right!=null){
+            this.right.midOrder();
+        }
+    }
+
+    //后序遍历
+    public void lastOrder(){
+        if(this.left!=null){
+            this.left.lastOrder();
+        }
+        if(this.right!=null){
+            this.right.lastOrder();
+        }
+        System.out.println(this);
+    }
+}
