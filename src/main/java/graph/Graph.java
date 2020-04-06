5049275d9a460da53816a6dd7455ddc3f051b958
Gavinwgq <henuboy@live.com>
2020-04-06 17:58:51 +0800

    图的深度优先遍历

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -13,6 +13,7 @@ public class Graph {
     private List<String> vertexs;//顶点集合
     private int[][] edges ;//邻接矩阵，边
     private int edgesnum;//边的个数
+    private boolean[] isVisted;//保存顶点是否被访问过
 
     public static void main(String[] args) {
         int n = 5;
@@ -28,12 +29,65 @@ public class Graph {
         graph.addEdge(1,4,1);//B-E
 
         graph.showGraph();
+
+        System.out.println("深度优先遍历");
+        graph.dfs();
     }
 
     public Graph(int n) {
         vertexs = new ArrayList<>();
         edges = new int[n][n];
         edgesnum = 0;
+        isVisted = new boolean[n];
+    }
+
+    /**
+     * 获取顶点的第一个邻接顶点
+     * @param index 顶点索引
+     * @return 第一个邻接顶点的索引
+     */
+    public int getFirstNeighbor(int index){
+        for (int i = 0; i < vertexs.size(); i++) {
+            if(edges[index][i] > 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接节点的下标获取下一个邻接节点
+     * @param v1 当前节点下标
+     * @param v2 当前节点邻接节点的下标
+     * @return 当前节点的下一个邻接节点下标
+     */
+    public int getNextNeighbor(int v1,int v2){
+        for (int i = v2+1; i < vertexs.size(); i++) {
+            if(edges[v1][i]>0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void dfs(boolean[] isVisted,int i){
+        System.out.print(getVertex(i)+"->");//输出该节点
+        isVisted[i] = true;//标记该节点被访问过了
+        int w = getFirstNeighbor(i);
+        while (w!=-1){
+            if(!isVisted[w]){
+                dfs(isVisted,w);
+            }
+            w = getNextNeighbor(i,w);
+        }
+    }
+
+    public void dfs(){
+        for (int i = 0; i < vertexs.size(); i++) {
+            if(!isVisted[i]){
+                dfs(isVisted,i);
+            }
+        }
     }
 
     /**
