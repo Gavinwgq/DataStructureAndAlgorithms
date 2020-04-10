fe889f9becdc3e63d8673745acb085fe9cc24226
Gavinwgq <henuboy@live.com>
2020-04-10 13:24:37 +0800

    KMP算法添加注释

--- src/main/java/Algorithms/kmp/KMPMatch.java
+++ src/main/java/Algorithms/kmp/KMPMatch.java
@@ -11,7 +11,7 @@ public class KMPMatch {
     public static void main(String[] args) {
         String str1 = "AABCABD CAABDAB BACDA";
         String str2 = "AABDAB";
-        int[] next = kmpNext(str2);
+        int[] next = buildNext(str2);
         System.out.println("next="+ Arrays.toString(next));
         int index = kmpMatch(str1, str2, next);
         System.out.println("index="+index);
@@ -32,12 +32,18 @@ public class KMPMatch {
         return -1;
     }
 
-    public static int[] kmpNext(String str){
+    /**
+     * 构建next数组
+     * @param str 要查找的字符串
+     * 求解过程参考：https://www.zhihu.com/question/21923021/answer/1032665486
+     * @return
+     */
+    public static int[] buildNext(String str){
         int[] next = new int[str.length()];
         next[0] = 0;
         for (int i = 1,j=0; i < str.length(); i++) {
             while (j>0 && str.charAt(i)!=str.charAt(j)){
-                j = next[j-1];
+                j = next[j-1];//核心点，重点理解
             }
             if(str.charAt(i) == str.charAt(j)){
                 j++;
